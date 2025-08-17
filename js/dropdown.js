3a5df4abadd9434d2c066a7fe401826f62fd62fd
Daniel Zamora Paez <[email>
2025-08-16 19:55:30 -0500

    Reestructuración de los archivos, se creacron nuevos archivos css y js para modular el codigo ya extenso

---
+++ js/dropdown.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // --- Lógica del Dropdown ---
+  const dropdownBtn = document.querySelector('.dropdown-btn');
+  if (dropdownBtn) {
+    dropdownBtn.addEventListener('click', function () {
+      const dropdownContent = document.querySelector('.dropdown-content');
+      if (dropdownContent) {
+        dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+      }
+    });
+  }
+});
